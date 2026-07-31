09ca6cf304450de2f5ca830bdf85035f2e84c4be
King21t <obaakinsola2000@gmail.com>
2026-07-31 21:04:27 +0100

    Add promoteToAdmin route to admin.routes.js

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { findUserByEmail, upgradeUser, downgradeUser } = require("../controllers/admin.controller");
+const { findUserByEmail, upgradeUser, downgradeUser, promoteToAdmin } = require("../controllers/admin.controller");
 
 const router = express.Router();
 
 router.post("/lookup", findUserByEmail);
 router.post("/upgrade", upgradeUser);
 router.post("/downgrade", downgradeUser);
+router.post("/promote", promoteToAdmin);
 
 module.exports = router;
